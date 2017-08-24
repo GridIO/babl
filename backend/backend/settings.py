fb8a80d8a2219f73ae29cf4a8abb4bbbf7ff876c
Haldun Anil <haldunanil@gmail.com>
2017-08-23 20:17:07 -0400

    Static_root transition part 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,13 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root/")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Media files
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root/")
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 
 # User model changes
 
